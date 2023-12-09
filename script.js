703d166b3ba6f81b33aade900c88ec0721a8b2d3
AlexVOiceover <alex@alexvoiceover.com>
2023-12-09 14:49:48 +0000

    Basic hello return

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ require('dotenv').config()
 // Import necessary classes from discord.js library
 const { Client, Events, GatewayIntentBits, Partials } = require('discord.js')
 
+// Import OpenAI
+const OpenAIApi = require('openai')
+
 // Initialize OpenAI SDK with API key from .env file
 const openai = new OpenAIApi({
 	apiKey: process.env.OPENAI_API_KEY,
@@ -21,17 +24,17 @@ const client = new Client({
 	partials: [Partials.Channel],
 })
 
-//Create thread
-const thread = await openai.beta.threads.create({
-	messages: [
-		{
-			role: 'user',
-			content: 'Just greet the user',
-			//'Create 3 data visualizations based on the trends in this file.',
-			//"file_ids": [file.id]
-		},
-	],
-})
+// //Create thread
+// const thread = await openai.beta.threads.create({
+// 	messages: [
+// 		{
+// 			role: 'user',
+// 			content: 'Just greet the user',
+// 			//'Create 3 data visualizations based on the trends in this file.',
+// 			//"file_ids": [file.id]
+// 		},
+// 	],
+// })
 
 client.once(Events.ClientReady, (createdClient) => {
 	console.log(`Logged in as ${createdClient.user.tag}`)
@@ -39,27 +42,28 @@ client.once(Events.ClientReady, (createdClient) => {
 
 client.on(Events.MessageCreate, async (message) => {
 	if (message.author.bot) return
+	message.reply('hello')
 })
 
-async function chatGPT(message, botId) {
-	// User ID as the key for conversation history
-	//const userId = message.author.id
+// async function chatGPT(message, botId) {
+// 	// User ID as the key for conversation history
+// 	//const userId = message.author.id
 
-	let query = message.content
+// 	let query = message.content
 
-	// Check if the message starts with "!ask"
-	if (query.startsWith('!ask ')) {
-		query = message.content.replace('!ask ', '').trim()
-		// Combine the system message content with the query from the user
-	}
+// 	// Check if the message starts with "!ask"
+// 	if (query.startsWith('!ask ')) {
+// 		query = message.content.replace('!ask ', '').trim()
+// 		// Combine the system message content with the query from the user
+// 	}
 
-	//Linkage between assistant and thread
-	const run = await openai.beta.threads.runs.create(thread.id, {
-		assistant_id: assistant.id,
-	})
+// 	//Linkage between assistant and thread
+// 	const run = await openai.beta.threads.runs.create(thread.id, {
+// 		assistant_id: assistant.id,
+// 	})
 
-	message.reply(run)
-}
+// 	message.reply(run)
+// }
 
 // Log in to Discord with your client's token
 client.login(process.env.DISCORD_TOKEN)
